13e776609ba54c637d7607552167e407f2715597
VivekDurgule12 <vdurgule31@gmail.com>
2024-07-10 18:17:51 +0530

    add file

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const fs = require('fs')
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "../public")))
 
 app.get("/", function(req, res) {
     fs.readdir(`./files`, function(err, files) {
