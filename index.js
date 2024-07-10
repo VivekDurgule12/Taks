f8dad991ef522773f599e314421a50f93f5187bd
VivekDurgule12 <vdurgule31@gmail.com>
2024-07-10 12:58:40 +0530

    add file

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post("/edit", function(req, res) {
 })
 
 app.post("/create", function(req, res) {
-    fs.writeFile(`. / files / $ { req.body.title.split(' ').join('') }.txt `, req.body.details, function(err) {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('') }.txt`, req.body.details, function(err) {
         res.redirect("/")
     })
 })
